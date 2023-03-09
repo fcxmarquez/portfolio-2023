32d8bee5fa3ce5fe934df1bc9bbe8d1f72ea5989
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-03-09 14:29:29 -0600

    Navbar animation

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import { NavWrapper } from "@/components/Nav/nav.styles";
+import Hamburger from "hamburger-react";
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <NavWrapper className="bg-black">
       <p className="font-primary font-bold text-white">Francisco Márquez</p>
-      <span className="hamburger bg-primary">
-        <div />
-        <div />
-        <div />
-      </span>
+      <div className="hamburger-icon bg-primary">
+        <Hamburger
+          distance="sm"
+          color="white"
+          rounded
+          toggled={isOpen}
+          toggle={setIsOpen}
+          size={18}
+        />
+      </div>
     </NavWrapper>
   );
 };
--- components/Nav/nav.styles.ts
+++ components/Nav/nav.styles.ts
@@ -13,40 +13,27 @@ export const NavWrapper = styled.nav`
   align-items: center;
   position: fixed;
   z-index: 1000;
+  padding-left: 1rem;
 
   p {
     font-size: 1rem;
   }
 
-  span {
+  .hamburger-icon {
     height: 100%;
     display: flex;
-    width: 2.75rem;
-    gap: 2px;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
-
-    div {
-      border-radius: 5px;
-      width: 30%;
-      height: 2px;
-      background-color: white;
-    }
   }
 
   @media (min-width: ${systemDesign.breakpoints.tablet}) {
-    span {
+    .hamburger-icon {
       width: 4rem;
-
-      div {
-        height: 3px;
-      }
     }
   }
 
   @media (min-width: ${systemDesign.breakpoints.desktop}) {
-    span {
+    .hamburger-icon {
       display: none;
     }
   }
