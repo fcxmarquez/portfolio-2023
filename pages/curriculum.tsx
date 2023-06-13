26152e7cbbb1131ed852a09873a62d93a114e41d
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-06-13 14:45:56 -0600

    feat: created curriculum page

---
+++ pages/curriculum.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import { CSSProperties, ReactElement } from "react";
+import { Layout } from "@/components/Layout/Layout";
+import { headerCSS } from "@/components/Nav/nav.styles";
+
+const mainStyles: CSSProperties = {
+  minHeight: "100vh",
+  minWidth: "100vw",
+  position: "absolute",
+  overflowX: "hidden",
+  paddingTop: headerCSS.heightMobile,
+};
+
+export default function Curriculum() {
+  return (
+    <>
+      <Head>
+        <title>Francisco Marquez</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={mainStyles}>
+        <iframe
+          src="https://drive.google.com/file/d/126dfJVNgIAlVknt6cS-JZs0W7HYnUw07/preview"
+          allow="autoplay"
+          title="Francisco Marquez's Curriculum"
+          style={{
+            position: "absolute",
+            height: "100%",
+            width: "100%",
+          }}
+        />
+      </main>
+    </>
+  );
+}
+
+// Get the layout Layout with the prop onlyNav set to true
+
+Curriculum.getLayout = (page: ReactElement) => <Layout onlyNav={true}>{page}</Layout>;
