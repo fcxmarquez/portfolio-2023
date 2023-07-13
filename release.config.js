5b9681a145d7408813824598425e225c76819abe
fcxmarquez <francisco.marquez.solt@gmail.com>
2023-07-13 15:06:20 -0600

    build: created an action for release drafter

---
+++ release.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "CHANGELOG.md",
+      },
+    ],
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        releaseDraft: true,
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
