1feefd592ef1e0710e0d971714d13e92d11b4f55
Francisco Márquez <francisco.marquez.solt@gmail.com>
2023-08-03 14:44:19 -0600

    Automatic merge

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents() {
+    setupNodeEvents(on, config) {
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
+      require("@cypress/code-coverage/task")(on, config);
       // implement node event listeners here
+      return config;
     },
   },
+  video: false,
 });
